8866a91a08b92916149dc05d29954a2c872377bc
Çağkan Mert Öztaş <cmoztas2@gmail.com>
2022-01-24 10:49:54 +0300

    Qr Code Scanner Base

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
+import { BarCodeScanner } from 'expo-barcode-scanner';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { Button, SafeAreaView, StyleSheet, Text, View } from 'react-native';
+import tw from 'twrnc';
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const status = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status.granted);
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>
+  } else if(hasPermission === false) {
+    return <Text>No access to camera</Text>
+  }
+
+  const handleBarCodeScanned = ({type, data}) =>{
+    setScanned(true);
+    alert(`Type: ${type}\nData: ${data}`)
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={tw`flex-1 items-center justify-center`}>
+      <BarCodeScanner 
+        style= {StyleSheet.absoluteFillObject}
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+      />
+      {scanned && 
+        <Button 
+          title={'Tap to scan again'}
+          onPress={() => {
+            setScanned(false);
+          }}
+        />
+      }
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
